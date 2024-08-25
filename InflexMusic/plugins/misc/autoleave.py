69d03723fc8411686ea3118a0db0838624d47fec
GOKU <dr084891@gmail.com>
2024-08-25 17:27:34 +0530

    Update autoleave.py

--- InflexMusic/plugins/misc/autoleave.py
+++ InflexMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOG_GROUP_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1001891687243
+                                and i.chat.id != -1001891687243
                             ):
                                 if left == 20:
                                     continue
